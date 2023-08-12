e78299bf1e2aba813b15d5afcd29d350bd496986
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2023-08-12 04:42:46 +0300

    Added setters and getters , and showInfo method.

--- Email.java
+++ Email.java
@@ -4,24 +4,26 @@ public class Email {
     private String firstName;
     private String lastName;
     private String password;
+    private String email;
+    private String companySuffix = "fake.com";
     private String department;
     private String alternativeEmail;
     private int defaultPasswordLength = 8;
-    private int mailboxCapacity;
+    private int mailboxCapacity = 350;
 
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println("Email Created Successfully! : " + firstName + " " + lastName);
 
         String dep = setDepartment();
         while (dep == "Error")
             dep = setDepartment();
         this.department = dep;
-        System.out.println("Department: " + this.department);
 
         this.password = generatePassword(9);
         System.out.println("The Password is: " + this.password);
+
+        this.email = this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@" + this.department + "." + this.companySuffix;
     }
     private String setDepartment() {
         int choice;
@@ -31,13 +33,13 @@ public class Email {
         if (choice == 1)
             return "HR";
         else if (choice == 2)
-            return "Development";
+            return "development";
         else if (choice == 3)
-            return "Sales";
+            return "sales";
         else if (choice == 4)
-            return "Accounting";
+            return "accounting";
         else if (choice == 0)
-            return "None";
+            return "none";
         else {
             System.out.println("Invalid Choice!!");
             return "Error";
@@ -51,5 +53,29 @@ public class Email {
             password[i] = passwordSet.charAt(k);
         }
         return new String(password);
+    }
+    public void setMailboxCapacity(int mailboxCapacity) {
+        this.mailboxCapacity = mailboxCapacity;
+    }
+    public void setAlternativeEmail(String alternativeEmail) {
+        this.alternativeEmail = alternativeEmail;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public int getMailboxCapacity() {
+        return this.mailboxCapacity;
+    }
+    public String getAlternativeEmail() {
+        return this.alternativeEmail;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    public String showInfo() {
+        return "Name: " + this.firstName + " " + this.lastName +
+                "\nDepartment: " + this.department +
+                "\nCompany Email: " + this.email +
+                "\nMailbox Capacity: " + this.mailboxCapacity + "MB";
     }
 }
--- EmailAdminstratorApplication.java
+++ EmailAdminstratorApplication.java
@@ -1,5 +1,6 @@
 public class EmailAdminstratorApplication {
     public static void main(String[] args) {
         Email ahmed = new Email("Ahmed", "Fatthi");
+        System.out.println(ahmed.showInfo());
     }
 }
